26c13f1b6f35ef31a46a0b65d369592329927de0
Dongdong Zhou <dzhou121@gmail.com>
2016-12-28 03:04:10 +0000

    added signature support

--- components/DocMsg.js
+++ components/DocMsg.js
@@ -38,7 +38,7 @@ class DocMsg extends Component {
 
         var style = {
             position: "absolute",
-            left: (docpos[1] + win.get("col")) * (fontSize / 2) - padding,
+            left: (docpos[1] + win.get("col") - (docmsg.indexOf("(") - 5)) * (fontSize / 2) - padding,
             bottom: ((win.get("height") - docpos[0]) + win.get("row")) * fontSize * lineHeight,
             padding: "4px 6px 4px 6px",
             maxWidth: 400,
@@ -55,17 +55,32 @@ class DocMsg extends Component {
         }
         var currentParam = ""
         var docHtml = <span>({funcParams})</span>
+        var paramHmtl = []
         if (funcParams.trim()) {
-            currentParam = funcParams.split(",")[doccom]
-            if (currentParam != undefined) {
-                currentParam = currentParam.trim()
-            }
+            funcParams.split(",").forEach((param, i) => {
+                var className = ""
+                if (i == doccom) {
+                    className = "doc-current"
+                }
+
+                var comma = ""
+                if (i > 0) {
+                    comma = ", "
+                }
+                param = param.trim()
+                paramHmtl.push(<span key={i}>{comma}<span className={className}>{param}</span></span>)
+            })
+            // currentParam = funcParams.split(",")[doccom]
+            // if (currentParam != undefined) {
+            //     currentParam = currentParam.trim()
+            // }
         }
         if (currentParam) {
             var parts = funcParams.split(currentParam)
-            docHtml = <div><span>({parts[0]}</span><span className="doc-current">{currentParam}</span><span>{parts[1]})</span></div>
+            // docHtml = <div><span>({parts[0]}</span><span className="doc-current">{currentParam}</span><span>{parts[1]})</span></div>
 
         }
+        docHtml = <div>{docmsg.slice(0, docmsg.indexOf("(") - 5)}({paramHmtl}){docmsg.slice(docmsg.indexOf(")") + 1)}</div>
         // console.log("current param is", currentParam)
         // var msg = JSON.parse(docmsg)
         //
